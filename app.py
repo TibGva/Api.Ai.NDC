b0eb731a9a36c719afe8a8d3dddc1d1bfce0d5a5
Tibo <ruyt@iata.org>
2017-03-18 16:42:32 +0100

    changed do request

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ from urllib.error import HTTPError
 import json
 import os
 
-import sys, httplib
+import sys
+import httplib
+import xml.dom.minidom
+
 HOST = "iata.api.mashery.com"
 API_URL = "/Zeus/NDC"
 
@@ -65,21 +68,28 @@ def processRequest(req):
 
 
 def do_request(xml_location):
-	"""HTTP XML Post request, by www.forceflow.be"""
-	request = open(xml_location,"r").read()
-    	webservice = httplib.HTTP(HOST)
-	webservice.putrequest("POST", API_URL)
-	webservice.putheader("Host", HOST)
-	webservice.putheader("User-Agent","Python post")
-	webservice.putheader("Content-type", "text/xml; charset=\"UTF-8\"")
-	webservice.putheader("Content-length", "%d" % len(request))
-	webservice.endheaders()
-	webservice.send(request)
-	statuscode, statusmessage, header = webservice.getreply()
-	result = webservice.getfile().read()
-        print statuscode, statusmessage, header
-        print result
+    """HTTP XML Post request"""
+    request = open(xml_location, "r").read()
+    webservice = httplib.HTTP(HOST)
+    webservice.putrequest("POST", API_URL)
+    webservice.putheader("Host", HOST)
+    webservice.putheader("User-Agent", "Python post")
+    webservice.putheader("Content-type", "text/xml; charset=\"UTF-8\"")
+    webservice.putheader("Content-length", "%d" % len(request))
+    webservice.endheaders()
 
+    webservice.send(request)
+
+    statuscode, statusmessage, header = webservice.getreply()
+
+    result = webservice.getfile().read()
+    resultxml = xml.dom.minidom.parseString(result)
+
+    print statuscode, statusmessage, header
+    print resultxml.toprettyxml()
+
+    with open("output-%s" % xml_location, "w") as xmlfile:
+        xmlfile.write(resultxml.toprettyxml())
 
 
 
